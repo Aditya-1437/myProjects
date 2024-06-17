7f445c8ea0ef2309d2d5bc69da64a57a80827baf
Aditya-1437 <159253199+Aditya-1437@users.noreply.github.com>
2024-06-17 10:43:52 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,80 @@
+var no = document.querySelectorAll(".drum").length;
+
+function handelClick(c){
+    switch (c) {
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            
+            break;
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            
+            break;
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            
+            break;
+        case "j":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            
+            break;
+        case "k":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            
+            break;
+        case "l":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currKey){
+    var activeButton = document.querySelector("."+currKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },300)
+}
+
+for(var i=0; i<no;i++){
+    document.querySelectorAll("button")[i].addEventListener('click',function(){
+        var buttonInnerHtml = this.innerHTML;
+        handelClick(buttonInnerHtml);
+        document.getElementById("play").innerHTML=buttonInnerHtml;
+        buttonAnimation(buttonInnerHtml);
+    })
+}
+document.addEventListener("keypress",function(event){
+    handelClick(event.key);
+    document.getElementById("play").innerHTML=event.key
+    buttonAnimation(event.key);
+})
+
+// function handelClick(){
+//     // var audio = new Audio('sounds/tom-1.mp3');
+//     // audio.play()
+//     var buttonInnerHtml = this.innerHTML;
+//     switch (key) {
+//         case value:
+            
+//             break;
+    
+//         default:
+//             break;
+//     }
+// }
